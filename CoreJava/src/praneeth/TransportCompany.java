88b5527b62114fe085d20ce8da39a24dff22eaad
saipro & pro <saipro & pro@sai.Dlink>
2021-01-28 12:15:10 +0530

    aggreagation

--- CoreJava/src/praneeth/TransportCompany.java
+++ CoreJava/src/praneeth/TransportCompany.java
@@ -18,7 +18,7 @@ package praneeth;
 		class TransportCompany{
 		   public static void main (String args[])
 		   {
-			Driver obj = new Driver("Andy", "Ford", 9988);
+			Driver obj = new Driver("praneeth", "benz", 12370);
 			System.out.println(obj.driverName+" is a driver of car Id: "+obj.carId);
 		   }
 		}
