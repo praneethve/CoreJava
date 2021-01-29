be1a6f7c484819486d6850d180c444a183dc9b9d
saipro & pro <saipro & pro@sai.Dlink>
2021-01-29 18:40:29 +0530

    polymorphism

---
+++ CoreJava/src/c8/Animal.java
@@ -0,0 +1,7 @@
+package c8;
+
+public class Animal{
+	   public void sound(){
+	      System.out.println("Animal is making a sound");   
+	   }
+	}
---
+++ CoreJava/src/c8/Cat.java
@@ -0,0 +1,12 @@
+package c8;
+
+class Cat extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Meow");
+    }
+    public static void main(String args[]){
+    	Animal obj = new Cat();
+    	obj.sound();
+    }
+}
