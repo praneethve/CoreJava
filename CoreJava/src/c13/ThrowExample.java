fff4b9a296bb95afb2125d7b2976fe6406fcfccd
saipro & pro <saipro & pro@sai.Dlink>
2021-02-03 20:08:44 +0530

    program

--- CoreJava/src/c13/ThrowExample.java
+++ CoreJava/src/c13/ThrowExample.java
@@ -11,7 +11,7 @@ public class ThrowExample {
 
 	   public static void main(String args[]){ 
 	     System.out.println("Welcome to the Registration process");
-	     checkEligibilty(10, 39); 
+	     checkEligibilty(10, 399); 
 	     System.out.println("Have a nice day"); 
 	 } 
 	}
