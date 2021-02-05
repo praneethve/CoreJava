37b9065231378eb3c31f956288b92627645aa1af
PRANEETH <PRANEETH@Pravalli-PC>
2021-02-05 09:32:38 +0530

    hmap changed

--- CoreJava/src/c18/Deatils.java
+++ CoreJava/src/c18/Deatils.java
@@ -14,24 +14,24 @@ public class Deatils {
       hmap.put(12, "mom");
       hmap.put(2, "dad");
       hmap.put(7, "sis");
-      hmap.put(49, "Bro");
+      System.out.println("string is" +hmap.put(49, "Bro"));
       hmap.put(3, "me");
-
+      System.out.println("string is" +hmap.put(49, "praneeth"));
       
-      Set set = hmap.entrySet();
+      Set set = hmap.keySet();
       Iterator iterator = set.iterator();
       while(iterator.hasNext()) {
-         Map.Entry mentry = (Map.Entry)iterator.next();
-         System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-         System.out.println(mentry.getValue());
+         Integer key =  (Integer)iterator.next();
+         System.out.print("key is: "+ key + " & Value is: ");
+         System.out.println(hmap.get(key));
       }
 
       
       String var= hmap.get(2);
       System.out.println("Value at index 2 is: "+var);
 
-      
-      hmap.remove(3);
+      System.out.println(" element 90 is: "+hmap.remove(90));
+      System.out.println(" element 3 is: "+hmap.remove(3));
       System.out.println("Map key and values after removal:");
       Set set2 = hmap.entrySet();
       Iterator iterator2 = set2.iterator();
