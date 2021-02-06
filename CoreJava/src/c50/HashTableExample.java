d42495ba12274d42721d078dbd1127217184c232
saipro & pro <saipro & pro@sai.Dlink>
2021-02-06 20:55:33 +0530

    hash table

--- CoreJava/src/c50/HashTableExample.java
+++ CoreJava/src/c50/HashTableExample.java
@@ -1,5 +1,31 @@
 package c50;
 
-public class HashTableExample {
+import java.util.Hashtable;
+import java.util.Enumeration;
 
+public class HashTableExample {
+ 
+ public static void main(String[] args) {
+ 
+   Enumeration names;
+   String key;
+ 
+ 
+   Hashtable<String, String> hashtable = 
+              new Hashtable<String, String>();
+ 
+   
+   hashtable.put("Key1","mom");
+   hashtable.put("Key2","dad");
+   hashtable.put("Key3","sis");
+   hashtable.put("Key4","bro");
+   hashtable.put("Key5","me");
+ 
+   names = hashtable.keys();
+   while(names.hasMoreElements()) {
+      key = (String) names.nextElement();
+      System.out.println("Key: " +key+ " & Value: " +
+      hashtable.get(key));
+   }
+ }
 }
