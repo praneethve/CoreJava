c86f49e9d209cc053946f3071851092e0605d357
saipro & pro <saipro & pro@sai.Dlink>
2021-02-07 14:07:48 +0530

    Queue example

---
+++ CoreJava/src/c51/QueueExample1.java
@@ -0,0 +1,32 @@
+package c51;
+
+import java.util.*;
+public class QueueExample1 {
+	 
+   public static void main(String[] args) {
+	  
+      
+      Queue<String> q = new LinkedList<String>();
+	    
+      
+      q.add("Praneeth");
+      q.add("Yewanth"); 
+      q.add("krishna");
+      q.add("maniratham");
+      q.add("avinash");
+	    
+      System.out.println("Elements in Queue:"+q);
+
+      System.out.println("Removed element: "+q.remove());
+	  
+      System.out.println("Head: "+q.element());
+	    
+      
+      System.out.println("poll(): "+q.poll());
+	    
+      
+      System.out.println("peek(): "+q.peek());
+	    
+            System.out.println("Elements in Queue:"+q);
+   }
+}
