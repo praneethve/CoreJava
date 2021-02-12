fcda238fd28af400eefa34e8e3dc447d537a401a
saipro & pro <saipro & pro@sai.Dlink>
2021-02-12 14:39:27 +0530

    Unboxing Examole

---
+++ CoreJava/src/c64/UnboxingExample1.java
@@ -0,0 +1,13 @@
+package c64;
+
+class UnboxingExample1
+{
+   public static void myMethod(int num){
+	System.out.println(num);
+   }
+   public static void main(String[] args) {
+    	
+    	Integer inum = new Integer(100);
+    	myMethod(inum);
+    }
+}
