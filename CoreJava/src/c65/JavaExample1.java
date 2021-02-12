eefb91e0fa7714c4f9aff564388faff3a3f65b34
saipro & pro <saipro & pro@sai.Dlink>
2021-02-12 19:50:04 +0530

    Wrapper Class

---
+++ CoreJava/src/c65/JavaExample1.java
@@ -0,0 +1,9 @@
+package c65;
+
+public class JavaExample1{  
+	   public static void main(String args[]){  
+		Integer obj = new Integer(99);  
+		int num = obj.intValue();
+		System.out.println(num+ " "+ obj);  
+	   }
+	}
