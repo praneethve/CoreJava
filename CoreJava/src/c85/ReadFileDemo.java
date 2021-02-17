c2cd30f277c98f7c90c2dd9ae6643b9794642b53
saipro & pro <saipro & pro@sai.Dlink>
2021-02-17 18:21:04 +0530

    Read File

---
+++ CoreJava/src/c85/ReadFileDemo.java
@@ -0,0 +1,47 @@
+package c85;
+
+import java.io.*;
+public class ReadFileDemo {
+   public static void main(String[] args) {         
+      
+      File file = new File("D://myfile.txt");
+      
+      BufferedInputStream bis = null;
+      FileInputStream  fis= null;
+
+      try
+      {
+          
+          fis = new FileInputStream(file);
+
+          
+          bis = new BufferedInputStream(fis);
+
+          
+          while( bis.available() > 0 ){             	
+              System.out.print((char)bis.read());
+          }
+
+       }catch(FileNotFoundException fnfe)
+        {
+            System.out.println("The specified file not found" + fnfe);
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("I/O Exception: " + ioe); 
+        }
+        finally
+        {
+            try{
+               if(bis != null && fis!=null)
+               {
+       	          fis.close();
+                  bis.close();
+               }      
+             }catch(IOException ioe)
+              {
+                  System.out.println("Error in InputStream close(): " + ioe);
+              }         
+        }
+   }    
+}
