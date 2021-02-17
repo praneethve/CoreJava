c464fe3377a5d792cbfe24ddd39751167becf072
saipro & pro <saipro & pro@sai.Dlink>
2021-02-17 18:15:06 +0530

    Create a file

---
+++ CoreJava/src/c84/CreateFileDemo.java
@@ -0,0 +1,25 @@
+package c84;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFileDemo
+{
+   public static void main( String[] args )
+   {	
+      try {
+	     File file = new File("D:\\newfile.txt");
+	     
+             boolean fvar = file.createNewFile();
+	     if (fvar){
+	          System.out.println("File has been created successfully");
+	     }
+	     else{
+	          System.out.println("File already present at the specified location");
+	     }
+    	} catch (IOException e) {
+    		System.out.println("Exception Occurred:");
+	        e.printStackTrace();
+	  }
+   }
+}
